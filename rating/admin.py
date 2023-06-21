285fcf63090ffd1a386dc209bf19867833bdef0e
alok kumar kaushal <lex.luther8765@gmail.com>
2023-06-21 16:40:49 +0530

    django web project

---
+++ rating/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from rating.models import *
+# Register your models here.
+admin.site.register(Thumbnail)
+admin.site.register(Round)
---
+++ rating/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("upload/", views.upload_image),
+    path("add/", views.add_thumbnail),
+    path("rank/", views.rankings),
+    path("delete/<int:thumb_id>/", views.delete_thumbnail),
+    path("", views.index),
+]
